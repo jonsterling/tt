c372d099eca0179dc881b3f26e520678814fbbf4
Jonathan Sterling <jon@jonmsterling.com>
2018-02-11 16:06:36 -0500

    add a bunch of OLEG-style elaborator code

--- src/lib/Elab.ml
+++ src/lib/Elab.ml
@@ -15,36 +15,66 @@ struct
     | Pair2
 end
 
-type state = Cut of Tm.chk * (Tm.chk -> state) option
+type state = Cut of Tm.ctx * Tm.chk * (Tm.chk -> state) option
 type tactic = state -> state
 
-let (<:) t k = Cut (t, Some k)
-let (<:?) t k = Cut (t, k)
-let ret t = Cut (t, None)
+let (<:) (ctx, tm) k = Cut (ctx, tm, Some k)
+let (<:?) (ctx, tm) k = Cut (ctx, tm, k)
 
 let up (st : state) : state =
   match st with
-  | Cut (t, Some k) -> k t
+  | Cut (ctx, tm, Some k) -> k tm
   | _ -> failwith "up"
 
 let down (addr : Addr.t) (st : state) : state =
   match addr, st with
-  | Pair1, Cut (Tm.Pair (t1, t2), kont) ->
-    t1 <: fun x1 ->
-      Tm.Pair (x1, t2) <:? kont
+  | Pair1, Cut (ctx, Tm.Pair (t1, t2), kont) ->
+    (ctx, t1) <: fun x1 ->
+      (ctx, Tm.Pair (x1, t2)) <:? kont
 
-  | Pair2, Cut (Tm.Pair (t1, t2), kont) ->
-    t2 <: fun x2 ->
-      Tm.Pair (t1, x2) <:? kont
+  | Pair2, Cut (ctx, Tm.Pair (t1, t2), kont) ->
+    (ctx, t2) <: fun x2 ->
+      (ctx, Tm.Pair (t1, x2)) <:? kont
 
   | _ -> failwith "down"
 
-let lift (f : Tm.chk -> Tm.chk) : tactic =
-  function Cut (t, kont) ->
-    f t <:? kont
+let lift (f : Tm.ctx -> Tm.chk -> Tm.chk) : tactic =
+  function Cut (ctx, t, kont) ->
+    (ctx, f ctx t) <:? kont
 
 let attack : tactic =
-  lift @@ fun t ->
+  lift @@ fun ctx t ->
     match t with
     | Tm.Hole (ty, bdy) -> guess ~ty:ty ~tm:(id_hole ty) ~bdy:bdy
     | _ -> failwith "attack"
+
+let lambda : tactic =
+  function Cut (ctx, t, kont) ->
+    match t with
+    | Tm.Hole (ty, Tm.Bind.Mk (Tm.Up Tm.Var)) ->
+      begin match NBE.nbe ctx ~tm:ty ~ty:Tm.U with
+      | Tm.Pi (dom, Tm.Bind.Mk cod) ->
+        (Tm.CExt (ctx, dom), id_hole ~ty:cod) <: fun z ->
+          (ctx, Tm.Lam (Tm.Bind.Mk z)) <:? kont
+      | _ -> failwith "lambda"
+      end
+    | _ -> failwith "lambda"
+
+let pi : tactic =
+  function Cut (ctx, t, kont) ->
+    match t with
+    | Tm.Hole (ty, Tm.Bind.Mk (Tm.Up Tm.Var)) ->
+      begin match NBE.nbe ctx ~tm:ty ~ty:Tm.U with
+      | Tm.U ->
+        (ctx, id_hole ~ty:U) <: fun dom ->
+          (Tm.CExt (ctx, dom), id_hole ~ty:U) <: fun cod ->
+             (ctx, Tm.Pi (dom, Tm.Bind.Mk cod)) <:? kont
+      | _ -> failwith "pi"
+      end
+    | _ -> failwith "pi"
+
+let (|>) (tac1 : tactic) (tac2 : tactic) st =
+  tac1 (tac2 st)
+
+let test =
+  attack |> lambda
--- src/lib/Elab.mli
+++ src/lib/Elab.mli
@@ -7,7 +7,7 @@ sig
 end
 
 type state
-type tactic = state -> state
+type tactic
 
 (* functions to navigate the proof tree *)
 val up : tactic
@@ -15,3 +15,11 @@ val down : Addr.t -> tactic
 
 (* from OLEG / Idris *)
 val attack : tactic
+val lambda : tactic
+val pi : tactic
+
+(* This stuff kind of works, but it doesn't seem compatible with the birectional term language;
+   that is, it seems more natural to store type information in the lambda, since otherwise we don't
+   know how to extend the context when descending through the proof state.
+
+   Need to figure out what's going on... *)
