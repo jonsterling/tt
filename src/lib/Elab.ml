a03c806ee9f2366f5fccc16da6d76b5d2c61edf5
Darin Morrison <freebroccolo@users.noreply.github.com>
2018-02-20 18:27:57 -0700

    Use Base.Hashtbl

--- src/lib/Elab.ml
+++ src/lib/Elab.ml
@@ -5,13 +5,13 @@ struct
   type hole = string
 
   (* Here is where things like hash keys and tags should be kept *)
-  type term = 
+  type term =
     | In of (int, term, subst) term_f
     | Ref of hole * subst
   and subst = InSb of (term, subst) subst_f
 end
 
-module Tm = 
+module Tm =
 struct
   type hole = Types.hole
   type term = Types.term
@@ -19,7 +19,7 @@ struct
 
   open Types
 
-  let meta alpha sb = 
+  let meta alpha sb =
     Ref (alpha, sb)
 
   let into tf = In tf
@@ -29,7 +29,7 @@ struct
 
   (* TODO: implement *)
   let subst ~sb ~tm : term =
-    match tm with 
+    match tm with
     | In tmf ->
       begin match tmf with
       | Var _ -> failwith ""
@@ -43,19 +43,19 @@ struct
     | Ref (key, sb') -> Ref (key, intoS @@ Cmp (sb, sb'))
 end
 
-module Elab : Elab 
+module Elab : Elab
   with type term = Tm.term
   and type subst = Tm.subst
   and type hole = Tm.hole
-= 
+=
 struct
   type hole = Tm.hole
   type term = Types.term
   type subst = Types.subst
 
-  module HT = Caml.Hashtbl.Make (String)
+  type env = (string, term jdg) Hashtbl.t
 
-  type env = term jdg HT.t
+  (* let mk_env: unit -> env = Hashtbl.create (module String) *)
 
   type 'a t = env -> 'a
 
@@ -64,20 +64,20 @@ struct
   let map = `Define_using_bind
 
   let rec alt (ms : 'a t list) : 'a t =
-    match ms with 
+    match ms with
     | [] -> failwith "No alternatives"
     | m::ms ->
-      fun env -> 
-        let env' = HT.copy env in
-        try m env with 
+      fun env ->
+        let env' = Hashtbl.copy env in
+        try m env with
         | _ -> alt ms env'
 
   let alloc jdg env =
     let key = "fresh" in (* TODO *)
-    HT.add env key jdg;
+    ignore @@ Hashtbl.add env ~key ~data:jdg;
     key
 
-  module Let_syntax = 
+  module Let_syntax =
   struct
     let bind = bind
   end
@@ -85,24 +85,26 @@ struct
   let ask ~ctx ~ty =
     let%bind key = alloc @@ Chk (ctx, Ask, ty) in
     return (key, Tm.meta key @@ Tm.intoS Id)
-  
-  let find key env =
-    HT.find env key
 
-  (* TODO: 
+  let find key env =
+    Hashtbl.find_exn env key (* FIXME: use optional version *)
+
+  (* TODO:
      - [ ] possibly typecheck the term
      - [ ] allow things other than Ask via unification
    *)
   let fill key tm env =
-    match HT.find env key with
-    | Chk (ctx, Ask, ty) -> HT.replace env key @@ Chk (ctx, Ret tm, ty) 
+    match Hashtbl.find_exn env key with
+    | exception Not_found -> failwith ""
+    | Chk (ctx, Ask, ty) -> Hashtbl.set env ~key ~data:(Chk (ctx, Ret tm, ty))
     | _ -> failwith ""
 
   let rec out t : (int, term, subst) term_f t =
-    match t with 
+    match t with
     | Types.In tf -> return tf
     | Types.Ref (key, sub) ->
       match%bind find key with
       | Chk (_, Ret t', _) -> out @@ Tm.subst ~sb:sub ~tm:t'
       | Chk (_, Ask, _) -> failwith "[out]: Term is a hole"
+
 end
