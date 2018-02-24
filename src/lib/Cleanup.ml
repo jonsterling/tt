07a00e96a7d163d14ba90c603c16c9e9b9cb8282
Jonathan Sterling <jon@jonmsterling.com>
2018-02-24 14:00:36 -0500

    renaming

--- src/lib/Cleanup.ml
+++ src/lib/Cleanup.ml
@@ -1,10 +1,10 @@
 (* In this file are some ideas to clean up the treatment of abstract syntax *)
 
-type 'term subst =
+type 'jdgerm subst =
   | Id
   | Wk
-  | Cmp of 'term subst * 'term subst
-  | Ext of 'term subst * 'term
+  | Cmp of 'jdgerm subst * 'jdgerm subst
+  | Ext of 'jdgerm subst * 'jdgerm
 
 type ('a, 'b) tensor = 'a * 'b subst
 
@@ -87,26 +87,26 @@ module type EnvMonad =
 sig
   type key
 
-  (* Semantically, 't should be a poset; we refer to its order as the "information order". *)
-  type ('t, 'a) t
+  (* Semantically, 'jdg should be a poset; we refer to its order as the "information order". *)
+  type ('jdg, 'a) t
 
-  val return : 'a -> ('t, 'a) t
-  val bind : ('t, 'a) t -> ('a -> ('t, 'b) t) -> ('t, 'b) t
+  val return : 'a -> ('jdg, 'a) t
+  val bind : ('jdg, 'a) t -> ('a -> ('jdg, 'b) t) -> ('jdg, 'b) t
 
   (* This is intended to be abstract. Sometimes to interface with other code outside the monad,
      we need a way to turn a monadic action into a value, inside the monad. This is helpful
      when dealing with higher order functions, such as in the case of pretty-printers. *)
-  type 't env
-  val get_env : ('t, 't env) t
-  val run : 't env -> ('t, 'a) t -> 'a
+  type 'jdg env
+  val get_env : ('jdg, 'jdg env) t
+  val run : 'jdg env -> ('jdg, 'a) t -> 'a
 
 
-  val find : key -> ('t, 'a) t
-  val alloc : key -> 't -> ('a, unit) t
+  val find : key -> ('jdg, 'a) t
+  val alloc : key -> 'jdg -> ('a, unit) t
 
-  (* INVARIANT: the update must be monotone in the sense of the information order on 't.
+  (* INVARIANT: the update must be monotone in the sense of the information order on 'jdg.
      Behavior is UNDEFINED when the update is not an improvement. *)
-  val improve : key -> 't -> ('a, unit) t
+  val improve : key -> 'jdg -> ('a, unit) t
 end
 
 module type ProofState =
