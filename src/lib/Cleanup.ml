c9f0515ae4044267c496f35ce45156f8ad364043
Jonathan Sterling <jon@jonmsterling.com>
2018-02-23 20:08:29 -0500

    benign but imperative deferred substitution

--- src/lib/Cleanup.ml
+++ src/lib/Cleanup.ml
@@ -82,15 +82,46 @@ struct
     | Wk -> Var (ix + 1)
 end
 
+module type ProofState =
+sig
+  type key
+
+  type ('t, 'a) t
+  val return : 'a -> ('t, 'a) t
+  val bind : ('t, 'a) t -> ('a -> ('t, 'b) t) -> ('t, 'b) t
+
+  val find : key -> ('t, 'a) t
+end
+
 (* This is a model with references to holes *)
-module Disect (S : Signature) : Model with type 'a f = 'a S.t =
+module Disect (M : ProofState) (S : Signature) :
+sig
+  include Model with type 'a f = 'a S.t
+
+  type hole = Ask | Ret of t
+  type jdg = {cx : t list; ty : t; hole : hole}
+  type 'a m = (jdg, 'a) M.t
+
+  val hole : M.key -> t
+
+  val out : t -> [`F of t f | `V of int] m
+end =
 struct
   type 'a f = 'a S.t
 
   type t =
     | Var of int
     | In of t S.t
-    | Ref of string * t subst
+    | Ref of [`Defer of M.key * t subst | `Done of t] ref
+  (* Wrapping the above in a reference to a sum lets me avoid
+     having to destructively update the environment in order to
+     make updates that memoize lookup-and-subst operations; these
+     are different from other updates to the environment in that they
+     contain no change in information. Better to deal with it locally! *)
+
+  type hole = Ask | Ret of t
+  type jdg = {cx : t list; ty : t; hole : hole}
+  type 'a m = (jdg, 'a) M.t
 
   let var i = Var i
 
@@ -106,7 +137,10 @@ struct
     | Id, _ -> t
     | _, Var i -> proj sb i
     | _, In tf -> In (S.map (fun i a -> subst @@ Clo (a, weaken i sb)) tf)
-    | _, Ref (key, sb') -> Ref (key, Cmp (sb, sb'))
+    | _, Ref r ->
+      match !r with
+      | `Defer (key, sb') -> Ref (ref @@ `Defer (key, Cmp (sb, sb')))
+      | `Done t -> subst @@ Clo (t, sb)
 
   and proj sb ix =
     match sb with
@@ -114,4 +148,25 @@ struct
     | Cmp (sb1, sb0) -> subst @@ Clo (proj sb0 ix, sb1)
     | Ext (_, t) -> if ix = 0 then t else proj sb (ix - 1)
     | Wk -> Var (ix + 1)
+
+  let rec out t =
+    match t with
+    | Var i -> M.return @@ `V i
+    | In tf -> M.return @@ `F tf
+    | Ref r ->
+      match !r with
+      | `Done t -> out t
+      | `Defer (key, sb) ->
+        M.bind (M.find key) @@ fun {hole;_} ->
+          match hole with
+          | Ask -> failwith "[out]: got Ask"
+          | Ret t ->
+            let t' = subst @@ Clo (t, sb) in
+            r := `Done t';
+            out t'
+
+
+  let hole key =
+    Ref (ref @@ `Defer (key, Id))
+
 end
